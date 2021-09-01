6d83411b709f2872b7c955eb413463ef4179e091
seyed <rseyedmoosavi@gmail.com>
2021-09-01 08:28:13 +0430

    Start My Project

---
+++ app/Http/Controllers/indexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class indexController extends Controller
+{
+    public function index($id){
+        return view('index',compact('id'));
+    }
+}
